6ae72106acc3d16d2499ae21c3249b6d599c60c6
omidashouri <omidashouri@gmail.com>
2021-10-13 22:57:07 +0330

    Initial commit : omidashouri : 000721

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/ir/omidashouri/site/ServletInitializer.java
@@ -0,0 +1,15 @@
+package ir.omidashouri.site;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//this is for War packaging
+public class ServletInitializer {
+                                /*extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShopmeFrontEndApplication.class);
+    }
+*/
+}
