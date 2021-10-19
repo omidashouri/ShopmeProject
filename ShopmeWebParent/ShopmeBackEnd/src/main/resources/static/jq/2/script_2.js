f44836fc0a9a86c4f832c94199859eb95ddef450
omidashouri <omidashouri@gmail.com>
2021-10-19 11:29:03 +0330

    Jquery>2.020 each loop advanced : omidashouri : 000727

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/jq/2/script_2.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/jq/2/script_2.js
@@ -1,10 +1,46 @@
 $(document).ready(function() {
+    //apply to the element which has id="output"
     $("#output").html("new content!");
 
-/*    var a = $("#Name").val();
-    var b = $("h2").html();
-    $("p").text(b);*/
-    
-    
-    
+    //apply all the elements which have class="one"
+    $(".one").html("new content!");
+
+    //apply all the paragraph "
+    $("p").html("new content!");
+
+    // write html then in response the 'content!' is at new line
+    // $("p").html("new <BR> content!");
+
+    // write text then in response the 'content!' is not at new line. result is 'new <BR> content!'
+    // $("p").text("new <BR> content!");
+
+    // with val we can manipulate value. like update, edit, or pic up value
+    // $("p").text("new <BR> content!");
+
+//    take the value of '#name' the put it as text for 'p'
+    /*var a = $("#Name").val();
+    $("p").text(a);*/
+
+//    take all the text inside h2 tag and put it inside p
+    /*   var b = $("h2").html();
+         $("p").text(b);*/
+
+    /*    $("li").each(function(){
+            console.log($(this).html());
+        });*/
+
+    $("li:eq(3)").html("number 3");
+
+    /*    $("li").each(function () {
+            $(this).after("AFTER");
+            $(this).append("APPEND");
+            $(this).before("BEFORE");
+            $(this).prepend("PREPEND");
+        })*/
+
+    $("li").each(function (index, event) {
+        console.log(index);
+        console.log(event);
+    });
+
 });
