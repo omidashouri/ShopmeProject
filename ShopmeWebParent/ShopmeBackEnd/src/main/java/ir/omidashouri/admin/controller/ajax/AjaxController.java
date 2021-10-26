e7e285d2ac9853d5cb5ebd17dc48978e24a0edf8
omidashouri <omidashouri@gmail.com>
2021-10-26 12:03:52 +0330

    AJAX>02.005 Dynamic Form field Check with AJAX JavaScriptFunction : omidashouri : 000803

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/ir/omidashouri/admin/controller/ajax/AjaxController.java
@@ -0,0 +1,44 @@
+package ir.omidashouri.admin.controller.ajax;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class AjaxController {
+
+
+//    http://localhost:8080/ShopmeAdmin/ajx/1
+
+    @GetMapping("/ajx/1")
+    public String viewHomePage() {
+        return "ajx/ajax_1";
+    }
+
+    @PostMapping("/ajx/checkName")
+    public String checkName(Model model, @Param("name") String name) {
+        String checkResult;
+        if (name != null) {
+            String receiveName = name;
+            List<String> archiveName = Arrays.asList("omid", "omid1", "omid2", "omid3");
+            if (receiveName.length() < 4) {
+                checkResult = "need to be more than 3 characters";
+            } else {
+                if (archiveName.contains(name)) {
+                    checkResult = "name is taken !!!!";
+                } else {
+                    checkResult = "name is available";
+                }
+            }
+        } else {
+            checkResult = "name is null";
+        }
+        model.addAttribute("checkResult", checkResult);
+        return "ajx/ajax_checkname_1";
+    }
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/ajx/ajx_1.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+
+    function checkServer() {
+        alert("salam 000000000000000");
+    }
+
+    /* var ajaxrequest;
+     var username = document.getElementById("username").value;
+     if (window.XMLHttpRequest) {
+         ajaxrequest = new XMLHttpRequest();
+     } else {
+         ajaxrequest = new ActiveXObject("Microsoft.XMLHTTP"); /// Older Browsers
+     }
+     ajaxrequest.onreadystatechange = function() {
+ console.log(ajaxrequest);*/
+    /*        if (ajaxrequest.readyState == 4 && ajaxrequest.status == 200) {
+                document.getElementById("result").innerHTML = ajaxrequest.responseText;
+            }*/
+    /*  }
+      ajaxrequest.open("POST", "http://localhost:8080/ShopmeAdmin/ajx/checkName", true);
+      ajaxrequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      ajaxrequest.send("name=" + username);*/
+
+});
