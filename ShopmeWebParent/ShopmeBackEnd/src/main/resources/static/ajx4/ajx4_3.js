9644ee392f46d345a0ba09d86831d3bc696422d8
omidashouri <omidashouri@gmail.com>
2021-10-30 23:17:03 +0330

    JQuery> 12 - Defining our request listener function - Part 1 AJAX, XMLHttpRequest and the Fetch() API : omidashouri : 000808

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/ajx4/ajx4_3.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/ajx4/ajx4_3.js
@@ -1,10 +1,35 @@
 function requestListener() {
 
+    //this is xhr
     console.log('this is what the request look like', this);
 
     let data = JSON.parse(this.responseText);
-    console.log(data);
+    console.log('the response text is now converted to JSON',data);
+    let user = data.results[0];
+    console.log('user looks like ',user);
 
+    //extract data we would like to use and display on our page
+    let fname = user.name.first;
+    let lname = user.name.last;
+    let src = user.picture.large;
+
+    //create our html elements
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
+
+    img.src = src;
+    p.innerHTML = `${fname} ${lname}`;
+
+    //append this items to out html
+    document.getElementById('user').appendChild(li);
+    li.appendChild(img);
+    li.appendChild(p)
+
+}
+
+function requestError(){
+    console.log('oops, i did it again');
 }
 
 
@@ -17,10 +42,11 @@ let url = "https://randomuser.me/api";
 let method = "GET";
 
 let xhr = new XMLHttpRequest();
-console.log('...', xhr);
+console.log('this is what our request looks like initially', xhr);
 
 xhr.open(method, url, true);
 
+//when the user has been retrived from server
 xhr.onload = requestListener;
 
 //onerror execute when we have error
