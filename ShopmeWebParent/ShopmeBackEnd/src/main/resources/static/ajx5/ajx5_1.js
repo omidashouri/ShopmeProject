e4487c357a2be7b76cecf994a4dd02371acfbded
omidashouri <omidashouri@gmail.com>
2021-11-07 15:42:22 +0330

    add callback : omidashouri : 000816

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/ajx5/ajx5_1.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/ajx5/ajx5_1.js
@@ -1,73 +1,114 @@
 $(document).ready(function () {
 
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    // https://sebtrif.xyz/blog/2019-10-03-client-side-ssl-in-node-js-with-fetch/
-let url = "https://api.imi.ir/edu-imi-ws/api/users/login";
-let customPost = {
-    username: 9057,
-    password: 1,
-    role: "anonymous"
-}
 
-let customHeader = {
-    "Content-Type": "application/json",
-    "Accept":"application/json",
-    "Access-Control-Expose-Headers":"Authorization, Origin, Content-Type, Accept, X-Auth-Token",
-    "access-control-allow-credentials": "true",
-    "access-control-allow-origin":"http://localhost:8080"
-}
+    //https://stackoverflow.com/questions/40981040/using-a-fetch-inside-another-fetch-in-javascript
+// let url = "https://api.imi.ir/edu-imi-ws/api/users/login";
+    let url = "https://ashouri-pc:8443/edu-imi-ws/api/users/login";
+    let customPost = {
+        username: 9057,
+        password: 1,
+        role: "anonymous"
+    }
+
+    let customHeader = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Access-Control-Expose-Headers": "Authorization, Origin, Content-Type, Accept, X-Auth-Token",
+        "access-control-allow-credentials": "true",
+        "access-control-allow-origin": "http://localhost:8080"
+    }
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
-    // headers.append('Host', 'api.imi.ir');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:8080');
     headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authentication');
     headers.append('Access-Control-Allow-Methods', 'POST, OPTIONS, FETCH');
 
 
-fetch(url, {
-    method: "POST",
-    headers: headers,
-    // host:'api.imi.ir',
-    // mode: 'no-cors',
-    // credentials: 'same-origin,include',
-    credentials: 'include',
-    agent: httpsAgent,
-    // body: JSON.stringify(customPost)
-    body: JSON.stringify({username: 9057, password:1619993814,role: 'anonymous'})
-})
-    .then(
-        function(resp) {
-        console.log(resp.status);
-        console.log(resp.statusCode);
-        console.log(JSON.stringify(resp.headers));
-       resp.headers.forEach(function(val, key) {
-            console.log(key + ' -> ' + val);});
-        console.log(resp.headers.get("authorization"));
+    let myauthorization;
 
-    }).then(() => console.log("salammmmmmmmmm"))
-    /*    .then(responseOne => responseOne.json())
-        // .then(responseTwo => console.log(responseTwo))
-        .then(function (responseTwo) {
-            let user = responseTwo.results[0];
-            console.log(user);
 
-            // for each user display user on screen
-            let li = document.createElement("li");
-            let img = document.createElement("img");
-            let p = document.createElement("p");
+    const resultPromise =
+        fetch(url, {
+            method: "POST",
+            // headers: headers,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Access-Control-Allow-Headers': '*',
+                'access-control-expose-headers': '*',
+                'Access-Control-Allow-Origin': 'http://localhost:8083/'
+            },
+            host: 'https://ashouri-pc:8443/',
+            Origin: 'http://localhost:8083/',
+            mode: 'cors',
+            credentials: 'same-origin',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify({username: 9057, password: 888, role: 'anonymous'})
+        })
+            .then(
+                function (resp) {
+                    console.log(resp.status);
 
-            //add a src attribute that we can see the picture
-            img.src = user.picture.large;
+                    resp.headers.forEach(function (val, key) {
+                        console.log(key + ' -> ' + val);
+                    });
+                    myauthorization = resp.headers.get("Authorization")
+                    console.log(resp.headers.get("Authorization"));
+                    console.log('MyAuthorization' + myauthorization);
 
-            //    lets add person nam on the image
-            p.innerHTML = `${user.name.first} ${user.name.last}`;
 
-            // lets add all elements to the dom
-            li.appendChild(img);
-            li.appendChild(p);
-            document.getElementById('user').appendChild(li);
-        })*/
-    .catch(error => console.log('oops there is an erro', error));
+                    urlInner = 'https://ashouri-pc:8443/edu-imi-ws/api/v1/users/nationalCode/0075175266';
+                    return fetch(urlInner, {
+                        method: "GET",
+                        headers: {
+                            'Authorization': myauthorization,
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json'
+                        }
+                    })
+                        .then(responseI => responseI.json())
+                        .then(responseBodyI => {
+                            console.log('The response body of the inner/nested request:');
+                            console.log(JSON.stringify(responseBodyI) + '\n\n');
+                            return responseBodyI;
+                        }).catch(err => {
+                            console.error('Failed to fetch - ' + urlInner);
+                            console.error(err);
+                        });
+
+
+                })
+
+
+
+
+            /*.then(responseOne => responseOne.json())
+                // .then(responseTwo => console.log(responseTwo))
+                .then(function (responseTwo) {
+                    let user = responseTwo.results[0];
+                    console.log(user);
+
+                    // for each user display user on screen
+                    let li = document.createElement("li");
+                    let img = document.createElement("img");
+                    let p = document.createElement("p");
+
+                    //add a src attribute that we can see the picture
+                    img.src = user.picture.large;
+
+                    //    lets add person nam on the image
+                    p.innerHTML = `${user.name.first} ${user.name.last}`;
+
+                    // lets add all elements to the dom
+                    li.appendChild(img);
+                    li.appendChild(p);
+                    document.getElementById('user').appendChild(li);
+                })*/
+            .catch(error => console.log('oops there is an error', urlOuter));
+
+
+    resultPromise.then(jsonResult => {
+        console.log('Result - the title is "' + JSON.stringify(jsonResult) + '".');
+    });
 
 });
